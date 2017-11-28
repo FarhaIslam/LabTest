49e935a588c9c4ee2d1213c2177b49827a9ce076
FarhaIslam <farhaislam999@gamil.com>
2017-11-27 19:41:25 -0800

    Smiley face complete

--- MyProgram/src/farha/ru/java/SmileyFace.java
+++ MyProgram/src/farha/ru/java/SmileyFace.java
@@ -1,5 +1,20 @@
 package farha.ru.java;
+import java.awt.*;
+import java.applet.*;
 
-public class SmileyFace {
+public class SmileyFace extends Applet {
+	
+	public void paint(Graphics g){
+			
+			g.setColor(Color.yellow);
+			g.fillOval(100,100,100,100);
+			g.setColor(Color.blue);
+			g.fillOval(120,125,20,30);
+			g.fillOval(160,125,20,30);
+			g.setColor(Color.red);
+			//g.fillArc(130, 165, 40, 20, 0, -180);
+			g.drawArc(130,165,40,20,0,-180);
 
+		
+	    }
 }
